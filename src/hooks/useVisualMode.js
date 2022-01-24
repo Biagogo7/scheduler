93ef66f55428470aebd8abcd7df2487296bd7eeb
Adeyemi <henry.atayero@gmail.com>
2022-01-24 13:28:33 -0600

    updating hooks

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,15 +1,40 @@
+import { useState } from "react";
+
+
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
-}
+ 
+    function transition (newMode, replace = false) {
+      
+      const historyArray = [...history]
+      
+
+      if (replace === true) {
+        historyArray.pop();
+      }
+
+      setMode(newMode);
+      setHistory([...historyArray, newMode])
+
+      // const historyArray = [...history]
+      // historyArray.push(newMode)
+      // setHistory(historyArray);
+
+    }
+
+    function back () {
+      if (history.length >= 2)  {
+        setMode(history[history.length-2])
+      }
+
+      const historyArray = [...history]
+      historyArray.pop();
+      setHistory(historyArray)      
+    }
 
 
-function useCustomHook(newmode) {
-  const [mode, setMode] = useState(initial);
-
-  setMode(newmode);
-
-  return { newmode };
-}
+    return { mode, transition, back };
+};
 
